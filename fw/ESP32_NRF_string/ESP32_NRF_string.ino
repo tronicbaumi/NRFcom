f0b4ad9f5e9b5daf0d45e748511532db296b69eb
tronicbaumi <christoph.baumgartner@gmx.at>
2023-12-22 19:39:55 +0100

    CSV string seperation working

--- fw/ESP32_NRF_string/ESP32_NRF_string.ino
+++ fw/ESP32_NRF_string/ESP32_NRF_string.ino
@@ -10,7 +10,8 @@ const byte address[6] = "1RF24"; // address / identifier
   int Lind=1;
   long lastMsg = 0;
   char dummy;
-  String data0,data1,data2,data3,data4;
+  String data0,data1,data2,data3,data4,data5;
+  String string_text;
 
 
 
@@ -34,38 +35,35 @@ void loop() {
   {
     char text[33] = {0}; 
     radio.read(&text, sizeof(text)-1);
-    Serial.print(text);Serial.println(sizeof(text));
+    //Serial.print(text);Serial.println(sizeof(text));
+
+    string_text = String(text);
+    //Serial.print(text);Serial.println(string_text.length());
 
 
 
-      Serial.println("======Data received======");
-      // for(int i=0;i<text.length(); i++)
-      // {
-      //   if(text.charAt(i)==',')
-      //   {
-      //     Lsub[Lind++]=i;
-      //   }
-      // }
-      // data0 = text.substring(Lsub[0],Lsub[1]);
-      // data1 = text.substring(Lsub[1]+1,Lsub[2]);
-      // data2 = text.substring(Lsub[2]+1,Lsub[3]);
-      // data3 = text.substring(Lsub[3]+1,Lsub[4]);
-      // data4 = text.substring(Lsub[4]+1,Lsub[5]);
-      // data5 = text.substring(Lsub[5]+1,text.length()-2);
-      // Lind=1;
-      text="";
-      Serial.println (data0);
-      Serial.println (data1);
-      Serial.println (data2);
-      Serial.println (data3);
-      // Serial.println (data4);
-      // Serial.println (data5);
-      Serial.println("==============================");
-
-
-
-
-
-
+    Serial.println("======Data received======");
+    for(int i=0;i<string_text.length(); i++)
+    {
+      if(string_text.charAt(i)==';')
+      {
+        Lsub[Lind++]=i;
+      }
+    }
+    data0 = string_text.substring(Lsub[0],Lsub[1]);
+    data1 = string_text.substring(Lsub[1]+1,Lsub[2]);
+    data2 = string_text.substring(Lsub[2]+1,Lsub[3]);
+    data3 = string_text.substring(Lsub[3]+1,Lsub[4]);
+    //data4 = string_text.substring(Lsub[4]+1,Lsub[5]);
+    //data5 = string_text.substring(Lsub[5]+1,sizeof(text)-2);
+    Lind=1;
+    string_text="";
+    Serial.println (data0);
+    Serial.println (data1);
+    Serial.println (data2);
+    Serial.println (data3);
+    //Serial.println (data4);
+    //Serial.println (data5);
+    Serial.println("==============================");
   }
 }
